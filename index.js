ad5cdfcd4f35948d87331c3aa0e03a5b32b9a4f9
laurent <htd.laurent@gmail.com>
2018-01-19 14:22:45 +0100

    enable require

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {};
+
+module.exports.SpinalDrive_App_list = require("./core/SpinalDrive_App_list");
+module.exports.SpinalDrive_App = require("./core/SpinalDrive_App");
+module.exports.SpinalDrive_Env = require("./core/SpinalDrive_Env");
