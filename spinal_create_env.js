81e8433e085aca82b3cd2e780a99b28cd5b6903b
laurent <htd.laurent@gmail.com>
2018-01-22 18:36:12 +0100

    use resolve-tree package

--- spinal_create_env.js
+++ spinal_create_env.js
@@ -112,6 +112,7 @@ function main() {
     basedir: process.cwd(),
     lookups: ['dependencies']
   };
+  console.log(opts);
   resolve.packages(["."], opts, function (err, tree) {
     if (err) return console.error(err)
 
