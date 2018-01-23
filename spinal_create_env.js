2eabf6a17888443a2b5e8d3eba3c9b0228c44856
laurent <htd.laurent@gmail.com>
2018-01-23 17:30:25 +0100

    copyRecursiveSync

--- spinal_create_env.js
+++ spinal_create_env.js
@@ -107,6 +107,21 @@ function concat_module(mod) {
 
 }
 
+function copyRecursiveSync(src, dest) {
+  var exists = fs.existsSync(src);
+  var stats = exists && fs.statSync(src);
+  var isDirectory = exists && stats.isDirectory();
+  if (exists && isDirectory) {
+    fs.mkdirSync(dest);
+    fs.readdirSync(src).forEach(function (childItemName) {
+      copyRecursiveSync(path.join(src, childItemName),
+        path.join(dest, childItemName));
+    });
+  } else {
+    fs.linkSync(src, dest);
+  }
+}
+
 function main() {
   var _root;
   create_browser_folder();
